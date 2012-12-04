75316475991a3c74639a53635381a2b9ac26bb73
Aaron Jensen <aaronjensen@gmail.com>
2012-12-04 14:24:13 -0800

    Use php recipe, make conf updatable

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,12 +8,29 @@
 #
 include_recipe "yum::epel"
 include_recipe "php-fpm::yum-remi"
+include_recipe "php"
 
-packages = %w{php php-fpm php-common php-cli php-pear}
+packages = %w{php-fpm php-common php-gd php-mysql php-mbstring php-xml php-mcrypt}
+
+node.default["php-fpm"]["conf_dir"] = node["php"]["conf_dir"]
 
 packages.each do |package|
   package package do
     action :upgrade
-    options "--enablerepo=remi"
+    notifies :restart, "service[php-fpm]"
   end
+end
+
+template "#{node['php-fpm']['conf_dir']}/php-fpm.conf" do
+  source "php-fpm.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :restart, "service[php-fpm]"
+end
+
+service "php-fpm" do
+  supports :start => true, :stop => true, :status => true, :restart => true, :reload => true
+  action [:start, :enable]
+  subscribes :restart, "template[#{node['php']['conf_dir']}/php.ini]"
 end
